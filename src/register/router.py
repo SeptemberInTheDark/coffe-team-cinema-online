a01cb0cea0e85c962442ce7b1a31c80c2e89f5b3
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2024-09-24 17:58:08 +0300

    edit router.py
    Валидация email телефона переписана в функцию user_registration

--- src/register/router.py
+++ src/register/router.py
@@ -1,4 +1,5 @@
 import os
+import re
 from fastapi import APIRouter, Depends, Form
 from fastapi.responses import JSONResponse
 from db import get_db
@@ -8,6 +9,8 @@ from src.Users.crud import UserCRUD
 from src.Users.schemas import UserCreate
 from src.Users.manager import UserHashManager
 from src.utils.logging import AppLogger
+
+from config import settings
 
 logger = AppLogger().get_logger()
 
@@ -20,24 +23,29 @@ router = APIRouter(
 
 @router.post("")
 async def user_registration(
-    username: str = Form(..., min_length=3),
-    password: str = Form(..., min_length=3),
-    email: str = Form(...),
-    phone: str = Form(...),
+        username: str = Form(..., min_length=3),
+        password: str = Form(..., min_length=3),
+        email: str = Form(...),
+        phone: str = Form(...),
 
-    db: AsyncSession = Depends(get_db)
+        db: AsyncSession = Depends(get_db)
 ):
     try:
+        if not re.match(settings.EMAIL_VALIDATOR, email):
+            logger.error('Некорректный email')
+            return JSONResponse(status_code=400, content={"error": "Некорректный email"})
+
+        if not re.match(settings.PHONE_VALIDATOR, phone):
+            logger.error('Некорректный номер телефона')
+            return JSONResponse(status_code=400, content={"error": "Некорректный номер телефона"})
+
         user_salt = os.urandom(32).hex()
         hashed_password = UserHashManager.hash_str(password, user_salt)
 
         existing_user = await UserCRUD.check_user(db, username, email, phone)
-
-
         if existing_user:
-            return JSONResponse(status_code=400, content={"error":"Пользователь с таким"
-                                                                  " именем или почтой уже существует."})
-
+            return JSONResponse(status_code=400,
+                                content={"error": "Пользователь с таким именем или почтой уже существует."})
 
         new_user = UserCreate(
             username=username,
@@ -47,11 +55,11 @@ async def user_registration(
         )
         user = await UserCRUD.create_user(db=db, user=new_user)
 
-
         if not user:
-            return JSONResponse(status_code=400, content={"error": "Ошибка при создании "
-                                                                   "пользователя, попробуйте еще раз ..."})
-        logger.info("user: ", user)
+            return JSONResponse(status_code=400,
+                                content={"error": "Ошибка при создании пользователя, попробуйте еще раз..."})
+        logger.info(f"Пользователь {user.username} успешно зарегистрирован")
+
         return JSONResponse(status_code=200, content={
             "success": True,
             "message": "Пользователь успешно зарегистрирован",
@@ -61,11 +69,8 @@ async def user_registration(
                 "phone": user.phone,
                 "email": user.email
             }
-            })
+        })
 
-    except Exception as http_exc:
-        logger.error(f'Ошибка при создании пользователя: {Exception}')
-        raise http_exc
-
-
-
+    except Exception as exc:
+        logger.error(f'Ошибка при создании пользователя: {exc}')
+        return JSONResponse(status_code=500, content={"error": "Внутренняя ошибка сервера"})
