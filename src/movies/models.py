3c49d9cc80293405beea0115dd2237e4530b9ea2
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2024-10-03 11:24:36 +0300

    add new model moves

--- src/movies/models.py
+++ src/movies/models.py
@@ -15,6 +15,7 @@ class Movie(BaseModel):
     id = Column(Integer, primary_key=True)
     title = Column(String(255), nullable=False)
     description = Column(Text, nullable=False)
+    photo = Column(String(255), nullable=False)
     release_year = Column(Integer, nullable=False)
     director = Column(String(255), nullable=False)
     actors = Column(JSON, nullable=False)
@@ -36,3 +37,13 @@ class Rating(BaseModel):
     movie_id = Column(Integer, ForeignKey(Movie.id), nullable=False)
     user_id = Column(Integer, ForeignKey(User.id), nullable=False)
     rating = Column(Integer, nullable=False)
+
+
+class Actor(BaseModel):
+    __tablename__ = 'actors'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    movie_id = Column(JSON, nullable=False)
+    description = Column(Text, nullable=False)
+    photo = Column(String(255), nullable=False)
+
