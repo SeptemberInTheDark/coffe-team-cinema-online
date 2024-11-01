e68d7889992b6e1bd9dd0aca053ed12ca469c65b
Solo-Cat <sir.noen@yandex.ru>
2024-11-01 22:51:23 +0300

    fix bugs with migrations

--- src/Users/models.py
+++ src/Users/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, Integer, Boolean, JSON, ForeignKey
 
-from db import BaseModel, engine
+from db import BaseModel
 
 class Role(BaseModel):
     __tablename__ = "roles"
@@ -23,6 +23,6 @@ class User(BaseModel):
     phone = Column(String(length=15), nullable=False, unique=True, index=True)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=False)
-    role_id = Column(Integer, ForeignKey(Role.id), nullable=False, default=2)
+    role_id = Column(Integer, ForeignKey(Role.id), nullable=True)
 
 
