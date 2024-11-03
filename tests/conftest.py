927c3c10173b5e0f770efb0daa04c3ea826047ad
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2024-11-03 11:36:15 +0300

    add tests movies

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,8 @@ import asyncio
 import pytest
 from typing import AsyncGenerator
 
-from fastapi.testclient import TestClient
-
+from fastapi import FastAPI
 from httpx import AsyncClient
-
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.pool import NullPool
@@ -14,44 +12,40 @@ from db import get_db, BaseModel
 from config import settings
 from app import app
 
+# Строка подключения к тестовой базе данных
+DATABASE_URL_TEST = "postgresql+asyncpg://postgres:postgres@localhost/test_db"
 
-DATABASE_URL_TEST = settings.DATABASE_URL_TEST
-metadata = BaseModel.metadata
-
+# Создание асинхронного движка и сессии
 engine_test = create_async_engine(DATABASE_URL_TEST, poolclass=NullPool)
 async_session_maker = async_sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
+metadata = BaseModel.metadata
 metadata.bind = engine_test
 
-
+# Переопределение зависимости для получения сессии
 async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
 
-
 app.dependency_overrides[get_db] = override_get_async_session
 
-
+# Фикстура для подготовки базы данных
 @pytest.fixture(autouse=True, scope='session')
 async def prepare_database():
     async with engine_test.begin() as conn:
-        await conn.run_sync(metadata.create_all)
-    yield
+        await conn.run_sync(metadata.create_all)  # Создание всех таблиц
+    yield  # Тесты выполняются здесь
     async with engine_test.begin() as conn:
-        await conn.run_sync(metadata.drop_all)
+        await conn.run_sync(metadata.drop_all)  # Удаление всех таблиц после тестов
 
-
-# SETUP
+# Фикстура для создания события цикла
 @pytest.fixture(scope='session')
 def event_loop(request):
-    """Create an instance of the default event loop for each test case."""
+    """Создание экземпляра цикла событий по умолчанию для каждого теста."""
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
 
-
-client = TestClient(app)
-
-
+# Асинхронный клиент для тестирования API
 @pytest.fixture(scope="session")
 async def ac() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(app=app, base_url="http://test") as ac:
