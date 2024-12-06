25583fe3b79057cf1f7525a7a3ac50c657477b7a
darkgooddack <ulyanaworkpage@gmail.com>
2024-12-06 20:44:12 +0300

    new models

--- backend/app/models/reward.py
+++ backend/app/models/reward.py
@@ -1,37 +1,44 @@
-from sqlalchemy import Column, Integer, ForeignKey
-from sqlalchemy.dialects.postgresql import VARCHAR
+from sqlmodel import Field, Relationship, SQLModel
 
-from app.core.init_db import BaseModel
-
-from .movie import Movie
-from .actor import Actor
+from backend.app.models.actor import Actor
+from backend.app.models.movie import Movie
+from typing import List
 
 
-class RewardActor(BaseModel):
+class RewardActor(SQLModel, table=True):
     __tablename__ = "reward_actor"
     __table_args__ = {"schema": "public"}
 
-    id = Column(Integer, primary_key=True, index=True)
-    actor_id = Column(Integer, ForeignKey("public.actor.id"), nullable=False)
-    reward_id = Column(Integer, ForeignKey("public.reward.id"),nullable=False)
-    year = Column(Integer, nullable=False)
+    id: int = Field(primary_key=True, index=True)
+    actor_id: int = Field(foreign_key="public.actor.id", nullable=False)
+    reward_id: int = Field(foreign_key="public.reward.id",nullable=False)
+    year: int = Field(nullable=False)
+
+    actor: "Actor" = Relationship(back_populates="rewards")
+    reward: "Reward" = Relationship(back_populates="actors")
 
 
-class RewardMovie(BaseModel):
+class RewardMovie(SQLModel, table=True):
     __tablename__ = "reward_movie"
     __table_args__ = {"schema": "public"}
 
-    id = Column(Integer, primary_key=True, index=True)
-    movie_id = Column(Integer, ForeignKey("public.movie.id"), nullable=False)
-    reward_id = Column(Integer, ForeignKey("public.reward.id"), nullable=False)
-    year = Column(Integer, nullable=False)
+    id: int = Field(primary_key=True, index=True)
+    movie_id: int = Field(foreign_key="public.movie.id", nullable=False)
+    reward_id: int = Field(foreign_key="public.reward.id", nullable=False)
+    year: int = Field(nullable=False)
+
+    movie: "Movie" = Relationship(back_populates="rewards")
+    reward: "Reward" = Relationship(back_populates="movies")
 
 
-class Reward(BaseModel):
+class Reward(SQLModel, table=True):
     __tablename__ = "reward"
     __table_args__ = {"schema": "public"}
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(Integer, nullable=False)
-    avatar = Column(Integer, nullable=False)
-    nomination = Column(VARCHAR, nullable=False)
+    id: int = Field(primary_key=True, index=True)
+    name: int = Field(nullable=False)
+    avatar: int = Field(nullable=False)
+    nomination: str = Field(nullable=False)
+
+    actors: List["RewardActor"] = Relationship(back_populates="reward")
+    movies: List["RewardMovie"] = Relationship(back_populates="reward")
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,65 +1,77 @@
-from sqlalchemy import Column, Integer, Boolean, JSON, ForeignKey, CHAR, Text
-from sqlalchemy.dialects.postgresql import VARCHAR
+from sqlmodel import Field, Relationship, SQLModel
+from typing import List, Optional
+from backend.app.models.movie import MovieRating, MovieComments
+from backend.app.models.news import NewsViews, NewsComments
 
-from app.core.init_db import BaseModel
-from .movie import Movie
 
-
-class Like(BaseModel):
+class Like(SQLModel, table=True):
     __tablename__ = "like"
     __table_args__ = {"schema": "public"}
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("public.user.id"), nullable=False)
-    movie_id = Column(Integer, ForeignKey("public.movie.id"), nullable=False)
-    like = Column(Boolean, default=False, nullable=False)
+    id: int = Field(primary_key=True, index=True)
+    user_id: int = Field(foreign_key="public.user.id", nullable=False)
+    movie_id: int = Field(foreign_key=("public.movie.id"), nullable=False)
+    like: bool = Field(default=False, nullable=False)
+
+    user: "User" = Relationship(back_populates="likes")
 
 
-class Favorite(BaseModel):
+class Favorite(SQLModel, table=True):
     __tablename__ = "favorite"
     __table_args__ = {"schema": "public"}
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("public.user.id"), nullable=False)
-    movie_id = Column(Integer, ForeignKey("public.movie.id"), nullable=True)
-    heart = Column(Boolean, default=False)
+    id: int = Field(primary_key=True, index=True)
+    user_id: int = Field(foreign_key="public.user.id", nullable=False)
+    movie_id = Field(foreign_key="public.movie.id", nullable=False)
+    heart: bool = Field(default=True, nullable=False)
+
+    user: "User" = Relationship(back_populates="favorites")
 
 
-class Role(BaseModel):
+class Role(SQLModel, table=True):
     __tablename__ = "role"
     __table_args__ = {"schema": "public"}
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(VARCHAR(length=50), nullable=False)
-    permissions = Column(JSON, nullable=True)
 
+    id: int = Field(primary_key=True, index=True)
+    name: str = Field(max_length=50, nullable=False)
+    permissions: Optional[dict] = Field(sa_column_kwargs={"nullable": True}) # JSON ????
 
-class SocialNetworks(BaseModel):
+    users: List["User"] = Relationship(back_populates="role")
+
+class SocialNetworks(SQLModel, table=True):
     __tablename__ = "social_networks"
     __table_args__ = {"schema": "public"}
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("public.user.id"), nullable=False)
-    name = Column(VARCHAR(length=50), nullable=True)
-    url = Column(Text, nullable=True)
+    id: int = Field(primary_key=True, index=True)
+    user_id: int = Field(foreign_key="public.user.id", nullable=False)
+    name: str = Field(max_length=50, nullable=False)
+    url: str = Field(nullable=False)
 
+    user: "User" = Relationship(back_populates="social_networks")
 
-class User(BaseModel):
+class User(SQLModel, table=True):
     __tablename__ = "user"
     __table_args__ = {"schema": "public"}
 
-    id = Column(Integer, primary_key=True, index=True)
-    avatar = Column(VARCHAR(length=255), nullable=True)
-    username = Column(VARCHAR(length=255), nullable=False)
-    first_name = Column(VARCHAR(length=255), nullable=True)
-    last_name = Column(VARCHAR(length=255), nullable=True)
-    email = Column(VARCHAR(length=255), nullable=False, unique=True, index=True)
-    phone = Column(VARCHAR(length=20), nullable=False, unique=True, index=True)
-    hashed_password = Column(Text, nullable=False)
-    is_active = Column(Boolean, default=False)
-    role_id = Column(Integer, ForeignKey("public.role.id"), nullable=True)
-    country = Column(VARCHAR(length=255), nullable=True)
-    gender = Column(CHAR(length=1), nullable=True)
+    id: int = Field(primary_key=True, index=True)
+    avatar: str | None = Field(max_length=255, nullable=True)
+    username: str = Field(max_length=255, nullable=False)
+    first_name: str | None = Field(max_length=255, nullable=True)
+    last_name: str| None = Field(max_length=255, nullable=True)
+    email: str = Field(max_length=255, nullable=False, unique=True, index=True)
+    phone: str = Field(max_length=255, nullable=False, unique=True, index=True)
+    hashed_password: str = Field(nullable=False)
+    is_active: bool = Field(default=True, nullable=False)
+    role_id: int = Field(foreign_key="public.user.id", nullable=False)
+    country: str | None = Field(max_length=255, nullable=True)
+    gender: str | None = Field(max_length=1, nullable=True)
 
-
-
+    role: Role = Relationship(back_populates="users")
+    social_networks: List[SocialNetworks] = Relationship(back_populates="user")
+    likes: List[Like] = Relationship(back_populates="user")
+    favorites: List[Favorite] = Relationship(back_populates="user")
+    comments: List["MovieComments"] = Relationship(back_populates="user")
+    ratings: List["MovieRating"] = Relationship(back_populates="user")
+    news_views: List["NewsViews"] = Relationship(back_populates="user")
+    news_comments: List["NewsComments"] = Relationship(back_populates="user")
