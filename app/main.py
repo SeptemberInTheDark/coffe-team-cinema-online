53767a2bab1867f5228ee3e8b67b4924c2854c80
darkgooddack <ulyanaworkpage@gmail.com>
2025-01-16 13:04:15 +0300

    RedirectResponse

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 import uvicorn
+from starlette.responses import RedirectResponse
 
 from app.core.run_app import register_app
 
 
 app = register_app()
 
+@app.get("/")
+def read_root():
+    return RedirectResponse(url="/api/v1/docs")
 
 if __name__ == '__main__':
     uvicorn.run(
